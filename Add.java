22a738b7ed40e5201f967f16c634aefa1b005eaf
skamit09 <skamit09@gmail.com>
2017-07-20 16:20:43 +0530

    add two numbers

---
+++ Add.java
@@ -0,0 +1,19 @@
+package com.ing.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Add {
+
+
+	int a, b;
+    Math(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int add() {
+        return a + b;
+    }
+}
+
+
